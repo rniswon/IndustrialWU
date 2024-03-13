488639c6f9283c942f6aea3819c2ac0e1903cea9
Cathy Chamberlin <cchamberlin@usgs.gov>
2024-03-13 16:24:19 -0400

    merged source-specific withdraw data from location file and monthly files

--- StateWUFormatingScripts/MA_Formatting.R
+++ StateWUFormatingScripts/MA_Formatting.R
@@ -30,13 +30,14 @@ locations <- with(MAdat, {
     `1. Facility Name and Address.xlsx`$Sheet1 %>% rename(FacilityName = Facility, WaterUse = `Water Use`), 
     `4. Indust_Comm Sources Address With Lat Long.xlsx`$Sources_Indust_Comm_With_Lat_Lo,
     by = c("FacilityName", "WaterUse"),
-    suffix = c("_option1", "_option2")
+    suffix = c("_SOURCE", "_OFFICE")
     )
 }) %>%
   rename(SITE_NAME = FacilityName, BASIN = `Major Basin`, CATEGORY = WaterUse) %>%
   mutate(SITE_NAME = gsub("[[:punct:]]", "", gsub("-.*$", "", SITE_NAME)),
          BASIN = gsub("^.*-", "", BASIN), 
-         CATEGORY = case_when(CATEGORY == "COMM" ~ "COM", CATEGORY == "INDUST" ~ "IND")) 
+         CATEGORY = case_when(CATEGORY == "COMM" ~ "COM", CATEGORY == "INDUST" ~ "IND"),
+         Town_SOURCE = str_to_upper(Town_SOURCE), Address_SOURCE = str_to_upper(Address_SOURCE)) 
 
 annualvalues <- map_dfr(
   MAdat$`2. Water Usage by Facility_2000-2018.xlsx`,
@@ -44,7 +45,7 @@ annualvalues <- map_dfr(
   ) %>%
   rename(CATEGORY = Use, SITE_NAME = `Facility Name`, 
          ANNUAL_WD_MGD = `Annual Withdrawal Rate (MGD:Millions of Gallons per Day)`,
-         BASIN = Basin) %>%
+         BASIN = Basin, Town_SOURCE = TOWN) %>%
   mutate(CATEGORY = case_when(
     CATEGORY == "Commercial" ~ "COM",
     CATEGORY == "INDUSTRIAL" ~ "IND"
@@ -53,52 +54,57 @@ annualvalues <- map_dfr(
   SITE_NAME = gsub("[[:punct:]]", "", gsub("-.*$", "", SITE_NAME))) %>%
    unique()
 
+
+formatMAdata <- function(x) {
+
+  if(any(grepl("Monthly Totals", x))) {
+    partA <- as.data.frame(t(x[c(1:8, 27:35),])[-1,]) %>% filter(!is.na(V1))
+    row.names(partA) <- NULL
+    names(partA) <- x$`Facility Withdrawal Report`[c(1:8, 27:35)]
+    
+    colx <- grep("Period", x)
+    rowx <- grep("Period", unlist(x[colx]))
+    
+    coly <- grep("Monthly Totals", x)
+    rowy <- grep("Monthly Totals", unlist(x[coly]))
+    
+    if(rowx == rowy) {
+      partB_names <- x[c((rowx-1):rowx), c(colx:(coly - 1))] 
+      partB_names_df <- data.frame(SourceID = t(partB_names)[,2], SourceName = t(partB_names)[,1]) %>%
+        filter(!is.na(SourceName))
+      row.names(partB_names_df) <- NULL
+      partB_raw <- x[c((rowx):(rowx + 13)), c(colx:(coly - 1))] 
+      names(partB_raw) <- c(partB_raw[1,])
+      
+      partB <- partB_raw %>% 
+        mutate(across(.cols = -Period, ~as.numeric(.))) %>%
+        filter(Period != "Period") %>%
+        pivot_longer(-Period, names_to = "SourceID") %>%
+        pivot_wider(id_cols = "SourceID", names_from = "Period", 
+                    values_from = "value", names_glue = "{Period}_mg") %>%
+        full_join(., partB_names_df, by = "SourceID")
+      
+    } else if(rowx != rowy) {stop("Unexpected Formatting")}
+    
+    partsAB <- bind_cols(partA, partB)
+    
+    return(partsAB)
+  }
+}
+
 monthlybysource2018 <- map_dfr(
   MAdat$`3.2 Water Withrawal by Sources_2018.xlsx`,
   ~{
     x <- .x
-    
-    if(any(grepl("Monthly Totals", x))) {
-      partA <- as.data.frame(t(x[c(1:8, 27:35),])[-1,]) %>% filter(!is.na(V1))
-      names(partA) <- x$`Facility Withdrawal Report`[c(1:8, 27:35)]
-      
-      colx <- grep("Monthly Totals", x)
-      rowx <- grep("Monthly Totals", unlist(x[colx]))
-      
-      partB <- as.data.frame(t(x[c((rowx + 1):(rowx + 13)), c(1, colx)])) %>% 
-        mutate(across(.cols = everything(), ~as.numeric(.))) %>%
-        filter(!is.na(V1))
-      names(partB) <- x$`Facility Withdrawal Report`[c((rowx + 1):(rowx + 13))]
-      
-      partsAB <- bind_cols(partA, partB)
-      rownames(partsAB) <- NULL
-      
-      partsAB
-    }
+    formatMAdata(x)
   }
 )
 
-monthlybysource2017 <- map_dfr(
+monthlybysource2017 <- future_map_dfr(
   MAdat$`3.1 Water Withdrawal by Sources_2017.xlsx`,
   ~{
     x <- .x
-    if(any(grepl("Monthly Totals", x))) {
-      partA <- as.data.frame(t(x[c(1:8, 27:35),])[-1,]) %>% filter(!is.na(V1))
-      names(partA) <- x$`Facility Withdrawal Report`[c(1:8, 27:35)]
-      
-      colx <- grep("Monthly Totals", x)
-      rowx <- grep("Monthly Totals", unlist(x[colx]))
-      
-      partB <- as.data.frame(t(x[c((rowx + 1):(rowx + 13)), c(1, colx)])) %>% 
-        mutate(across(.cols = everything(), ~as.numeric(.))) %>%
-        filter(!is.na(V1))
-      names(partB) <- x$`Facility Withdrawal Report`[c((rowx + 1):(rowx + 13))]
-      
-      partsAB <- bind_cols(partA, partB)
-      rownames(partsAB) <- NULL
-      
-      partsAB
-    }
+    formatMAdata(x)
   }
 )
 
@@ -108,17 +114,17 @@ monthlybysource <- bind_rows(monthlybysource2017, monthlybysource2018) %>%
   rename(CATEGORY = `Water Use`, SITE_NAME = `Facility Name`, 
          ANNUAL_WD_MGD = AverageDailyWithdrawalVolume, 
          PERMIT_NUM = `Permit Number`, REG_NUM = `Registration Number`,
-         TOWN = Town, BASIN = Basin, YEAR = `Reporting Year`) %>%
+         Town_SOURCE = Town, BASIN = Basin, YEAR = `Reporting Year`) %>%
   mutate(
     CATEGORY = case_when(CATEGORY == "COMM" ~ "COM", CATEGORY == "INDUST" ~ "IND"),
-    ANNUAL_WD_MGD = round(as.numeric(ANNUAL_WD_MGD), 2), TOWN = str_to_upper(TOWN),
+    ANNUAL_WD_MGD = round(as.numeric(ANNUAL_WD_MGD), 2), Town_SOURCE = str_to_upper(Town_SOURCE),
     YEAR = as.numeric(YEAR),
     SITE_NAME = gsub("[[:punct:]]", "", gsub("-.*$", "", SITE_NAME))
   )
 
 MA_dat_all <- merge(
   annualvalues, monthlybysource, 
-      by = c("REG_NUM", "PERMIT_NUM", "TOWN", "YEAR", "SITE_NAME"), 
+      by = c("REG_NUM", "PERMIT_NUM", "Town_SOURCE", "YEAR", "SITE_NAME"), 
   all = TRUE, suffixes = c("_ANNREP", "_MONREP")) %>%
   mutate(CATEGORY = 
            case_when(!is.na(CATEGORY_ANNREP) ~ CATEGORY_ANNREP, 
@@ -127,12 +133,26 @@ MA_dat_all <- merge(
            case_when(!is.na(BASIN_ANNREP) ~ BASIN_ANNREP, 
                      TRUE ~ BASIN_MONREP)) %>%
   select(-c(CATEGORY_ANNREP, CATEGORY_MONREP, BASIN_ANNREP, BASIN_MONREP)) %>%
-  group_by(PERMIT_NUM, REG_NUM, TOWN) %>%
+  group_by(PERMIT_NUM, REG_NUM, Town_SOURCE) %>%
   fill(c(CATEGORY, BASIN), .direction = "downup") %>%
-  merge(., locations, by = c("SITE_NAME", "BASIN", "CATEGORY"), all = TRUE, suffix = c("", "_LOC"))
+  merge(., locations, by = c("SITE_NAME", "BASIN", "CATEGORY", "Town_SOURCE"), all = TRUE, suffix = c("_MONREP", "_LOC")) %>%
+  mutate(SourceID = case_when(
+    SourceID_MONREP == SourceID_LOC ~ SourceID_MONREP,
+    is.na(SourceID_MONREP) ~ SourceID_LOC,
+    is.na(SourceID_LOC) ~ SourceID_MONREP,
+    TRUE ~ "DROP"
+  ),
+  SourceName = case_when(
+    SourceName_MONREP == SourceName_LOC ~ SourceName_MONREP,
+    is.na(SourceName_MONREP) ~ SourceName_LOC,
+    is.na(SourceName_LOC) ~ SourceName_MONREP,
+    TRUE ~ "DROP"
+  )) %>% 
+  filter(SourceID != "DROP", SourceName != "DROP") %>%
+  select(-c(SourceID_MONREP, SourceID_LOC, SourceName_MONREP, SourceName_LOC)) 
 
 
-MA_dat_all %>% group_by(PERMIT_NUM, REG_NUM, YEAR, TOWN) %>% summarize(n = n()) %>% filter(n > 1)
+MA_dat_all %>% group_by(PERMIT_NUM, REG_NUM, YEAR, SourceID) %>% summarize(n = n()) %>% filter(n > 1)
 
 # Join data ----
 if(outputcsv) {write.csv(
