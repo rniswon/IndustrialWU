5a0226db8d2f8af7808880e15bd1c5753f13d3f0
Cathy Chamberlin <cchamberlin@usgs.gov>
2024-04-10 13:23:41 -0400

    Summarize Lightbox data

--- X_LightboxSummaries.R
+++ X_LightboxSummaries.R
@@ -3,7 +3,8 @@
 # Date: 2024-03-26
 # Contact: cchamberlin@usgs.gov (Cathy Chamberlin)
 ### ### ### ### ### ### ###
-
+library(tictoc)
+tic()
 # Setup ----
 # browser()
 optloadinstall <- function(x) {
@@ -38,6 +39,20 @@ USA_LULC <- map2_dfr(states, file.path(path_to_LBdata, state_zips),
      ~{
        message(paste(.x))
        unzipped_fp <- LB_unzipdata(.y, desired_outpath, TRUE)
-       stripped_dat <- LB_loadandstripData(unzipped_fp, .x)
-       dat_summary <- LB_summarizelanduse(stripped_dat, .x)
-     })
+       if(length(unzipped_fp) > 0) {
+         stripped_dat <- LB_loadandstripData(unzipped_fp, .x)
+       } else(stripped_dat <- data.frame())
+       if(nrow(stripped_dat) > 0) {
+         dat_summary <- LB_summarizelanduse(stripped_dat, .x)
+       } else {dat_summary <- data.frame()}
+       dat_summary
+     }) %>% tidyr::pivot_wider(id_cols = Var1, names_from = State, values_from = Freq) %>%
+  dplyr::rename(LU_Description = Var1) %>%
+  dplyr::mutate(LU_Description = as.character(LU_Description)) %>%
+  dplyr::mutate(LU_Description = dplyr::case_when(LU_Description == "<NA>" ~ "Unclassified",
+                                                  LU_Description != "<NA>" ~ LU_Description)) %>%
+  dplyr::mutate(across(-LU_Description, ~tidyr::replace_na(., 0))) %>%
+  dplyr::arrange(LU_Description)
+
+write.csv(USA_LULC, file = file.path(desired_outpath, "USA_LU_summaries.csv"), row.names = FALSE)
+toc()
--- utility_functions/SummarizeSFLightBox.R
+++ utility_functions/SummarizeSFLightBox.R
@@ -12,18 +12,23 @@ LB_loadandstripData <- function(path, state) {
   
   future::plan(future::multisession, workers = 4)
   
-  strippeddata <- future_map_dfr(c(1:ceiling(nfeatures / 100000)),
-                                 ~{
-                                   options(scipen = 10000000)
-                                   offset <- paste(signif((.x - 1) * 100000, 7))
-                                   query <- paste0("SELECT * FROM Assessments LIMIT 100000 OFFSET ", 
-                                                   (.x - 1) * 100000)
-                                   dattmp <- st_read(fp, "Assessments", query = query, quiet = TRUE) %>% 
-                                     st_drop_geometry()
-                                   dattmp
-                                 },
-                                 .progress = TRUE, seed = NULL) |>
-    dplyr::mutate(State = state)
+  if(nfeatures > 0) {
+    strippeddata <- future_map_dfr(c(1:ceiling(nfeatures / 100000)),
+                                   ~{
+                                     options(scipen = 10000000)
+                                     offset <- paste(signif((.x - 1) * 100000, 7))
+                                     query <- paste0("SELECT * FROM Assessments LIMIT 100000 OFFSET ", 
+                                                     (.x - 1) * 100000)
+                                     dattmp <- st_read(fp, "Assessments", query = query, quiet = TRUE) %>% 
+                                       st_drop_geometry()
+                                     dattmp
+                                   },
+                                   .progress = TRUE, seed = NULL) |>
+      dplyr::mutate(State = state)
+  } else {
+    strippeddata <- data.frame()
+  }
+  
   
   return(strippeddata)
   
@@ -31,21 +36,21 @@ LB_loadandstripData <- function(path, state) {
 
 LB_summarizelanduse <- function(dataframe, state) {
   if(!"USE_CODE_STD_DESC_LPS" %in% names(dataframe)) {stop("Data frame does not have a column `USE_CODE_STD_DESC_LPS`")}
-  as.data.frame(table(dataframe$USE_CODE_STD_DESC_LPS)) %>% dplyr::mutate(State = state)
+  as.data.frame(table(dataframe$USE_CODE_STD_DESC_LPS, useNA = "always")) %>% dplyr::mutate(State = state)
 }
 
 LB_unzipdata <- function(inputfile, output_dir, use_cache = TRUE) {
-
+# browser()
   state <- str_extract(inputfile, "(?<=Professional_)[[:upper:]]{2}")
   if(any(grepl(state, stringr::str_extract(list.dirs(output_dir), "(?<=Professional_)[[:upper:]]{2}")))) {
     if(use_cache == TRUE) {
       fpout <- list.dirs(output_dir, full.names = TRUE)[
         which(grepl(state, stringr::str_extract(list.dirs(output_dir),  "(?<=Professional_)[[:upper:]]{2}")))]
       } else {
-      archive::archive_extract(inputfile, dir = output_dir)
+        tryCatch(archive::archive_extract(inputfile, dir = output_dir), error = function(e) e)
           fpout <- list.dirs(output_dir, full.names = TRUE)[
             which(grepl(state, stringr::str_extract(list.dirs(output_dir),  "(?<=Professional_)[[:upper:]]{2}")))]}} else {
-          archive::archive_extract(inputfile, dir = output_dir)
+          tryCatch(archive::archive_extract(inputfile, dir = output_dir), error = function(e) e)
           fpout <- list.dirs(output_dir, full.names = TRUE)[
             which(grepl(state, stringr::str_extract(list.dirs(output_dir),  "(?<=Professional_)[[:upper:]]{2}")))]
         }
