262ee19913f4794323ce98d7ef5c4026beb59a0b
Cathy Chamberlin <cchamberlin@usgs.gov>
2024-06-28 14:49:49 -0400

    Incorporate WI

--- utility_functions/View_target_results.R
+++ utility_functions/View_target_results.R
@@ -4,3 +4,17 @@ View_state <- function(data, state) {
   View(tmp)
   glimpse(tmp)
 }
+
+View_old_new_STdata <- function(state, fp) {
+  tar_load(reformatted_data)
+  tar_load(datafp)
+  bee_old <- filter(reformatted_data, State == state)
+  statedat <- loadSTdata(file.path(datafp, state))
+  bee_new <- eval(parse(text = gsub(paste0("/", state, "/"), "statedat$", fp)))
+  
+  bee_hive <- list(ingested_dat = bee_old, new_dat = bee_new)
+  imap(bee_hive, ~{
+    eval(parse(text = paste0("View(bee_hive[['", .y, "']])")))
+    })
+  return(bee_hive)
+}
